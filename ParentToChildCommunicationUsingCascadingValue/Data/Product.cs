bdd6bda669ad8c24ef4a633f4d3202b63519197a
Netcode-Hub <Rastinga1995@hotmail.com>
2023-11-09 14:29:44 +0000

    Add project files.

---
+++ ParentToChildCommunicationUsingCascadingValue/Data/Product.cs
@@ -0,0 +1,8 @@
+namespace ParentToChildCommunicationUsingCascadingValue.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
